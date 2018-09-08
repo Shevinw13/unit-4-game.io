d8c2740167f4152a1dea8a0a2c34c2871434face
Shevin Weinstein <shevinweinstein1@gmail.com>
2018-09-08 10:10:51 -0400

    html and css

---
+++ javascript/game.js
@@ -0,0 +1,25 @@
+//var targetNumber = [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 80, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+
+//var crystalNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+$(document).ready(function() {
+
+    var crystalNumber1 = Math.floor(Math.random() * 13)
+    var crystalNumber2 = Math.floor(Math.random() * 13)
+    var crystalNumber3 = Math.floor(Math.random() * 13)
+    var crystalNumber4 = Math.floor(Math.random() * 13)
+    
+    var counter = 0;
+
+    var targetNumber = Math.floor(Math.random() * 51) + 50;
+
+    console.log(crystalNumber1)
+    console.log(crystalNumber2)
+    console.log(crystalNumber3)
+    console.log(crystalNumber4)
+    console.log(targetNumber)
+
+
+
+
+})
